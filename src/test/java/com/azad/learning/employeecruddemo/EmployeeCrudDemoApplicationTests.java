acc83a6030acc050b99f3230cdfa9ed8eea03b61
Azhar Uddin Azad <azhar.u.azad@gmail.com>
2020-03-12 14:57:29 +0600

    database create

---
+++ src/test/java/com/azad/learning/employeecruddemo/EmployeeCrudDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azad.learning.employeecruddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeCrudDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
