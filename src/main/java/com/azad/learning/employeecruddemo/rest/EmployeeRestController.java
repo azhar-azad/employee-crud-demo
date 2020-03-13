81ecec03d81dff84dd62a568cfeba999ddea5229
Azhar Uddin Azad <azhar.u.azad@gmail.com>
2020-03-13 13:21:33 +0600

    v1: get/id, post implemented

--- src/main/java/com/azad/learning/employeecruddemo/rest/EmployeeRestController.java
+++ src/main/java/com/azad/learning/employeecruddemo/rest/EmployeeRestController.java
@@ -4,9 +4,7 @@ import com.azad.learning.employeecruddemo.dao.EmployeeDAO;
 import com.azad.learning.employeecruddemo.entity.Employee;
 import com.azad.learning.employeecruddemo.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,5 +23,32 @@ public class EmployeeRestController {
     @GetMapping(path = "/employees")
     public List<Employee> findAll() {
         return employeeService.findAll(); 
+    }
+
+    // add mapping for GET /employees/{employeeId}
+    @GetMapping("/employees/{employeeId}")
+    public Employee getEmployee(@PathVariable int employeeId) {
+
+        Employee employee = employeeService.findById(employeeId);
+
+        if (employee == null) {
+            throw new RuntimeException("Employee id not found - " + employeeId);
+        }
+
+        return employee;
+    }
+
+    // add mapping for POST /employees - add new employee
+    @PostMapping("/employees")
+    public Employee addEmployee(@RequestBody Employee employee) {
+
+        // also just in case they pass an id in JSON .... set id to 0
+        // this is to force a save of new item ... instead of update
+
+        employee.setId(0);
+
+        employeeService.save(employee);
+
+        return employee;
     }
 }
